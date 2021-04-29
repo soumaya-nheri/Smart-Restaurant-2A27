0b385ed2d96a5d957fc101bef6fab2ec61efd1e3
Ahmed <ahmed.tlili@esprit.tn>
2021-04-30 00:52:27 +0100

    commit 12

--- login.cpp
+++ login.cpp
@@ -26,11 +26,15 @@ void MainWindow::on_pushButton_clicked()
     QString username =ui-> lineEdit_username->text();
 
     QString mdp=ui-> lineEdit_mdp->text();
+    QSqlQuery qry;
 
-    if (username == "admin" &&  mdp == "admin")
+    if (qry.exec("select * from personnel where NOM='"+username+ "'and MOT_DE_PASSE='"+mdp+"'"))
     {
-
-
+int count=0;
+while(qry.next()){
+    count ++;
+                 }
+if (count==1 or (username=="admin" && mdp=="admin") ){
              this->hide();
             gestion_personnel menu;
            menu.setModal(true);
@@ -39,10 +43,10 @@ void MainWindow::on_pushButton_clicked()
          }
 
 
-else {
+       if (count<1) {
         ui->label_connecting->setText("Nom d'utilisateur ou Mot de passe incorrect");
         ui->lineEdit_username->setText("");
         ui->lineEdit_mdp->setText("");
 }
 }
-
+}
